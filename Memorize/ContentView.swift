4c7293486eb688210556e594807293a415dcfd94
Michael Lee <michael@dss.com.mm>
2022-07-23 12:03:22 +0630

    added card containers

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView(content: "🚕")
+            CardView(content: "🚄")
+            CardView(content: "✈️")
+            CardView(content: "🚀")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    
+    var content: String = "Emoji"
+    @State var isFaceUp: Bool = true
+    
+    
+    var body: some View {
+        
+        ZStack {
+            
+            let Shape = RoundedRectangle(cornerRadius: 30)
+            if isFaceUp {
+                Shape.fill().foregroundColor(.white)
+                Shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle).padding()
+            }
+            else{
+                Shape
+                    .fill()
+            }
+            
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.dark   )
     }
 }
